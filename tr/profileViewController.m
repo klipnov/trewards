e2e71cb8eb5fba4b3813b1f8e2601625d7f9f370
Aliff Ali Azizan <tonywahles@Aliffs-MacBook-Pro.local>
2014-09-18 03:50:17 +0800

    sync

--- tr/profileViewController.m
+++ tr/profileViewController.m
@@ -8,9 +8,23 @@
 
 #import "profileViewController.h"
 #import <FacebookSDK/FacebookSDK.h>
+#import "AFNetworking/AFNetworking.h"
+#import "UIImageView+AFNetworking.h"
 
 @interface profileViewController ()
+@property (weak, nonatomic) IBOutlet UILabel *username;
+@property (weak, nonatomic) IBOutlet UIImageView *profilePic;
+@property (weak, nonatomic) IBOutlet UILabel *pointLabel;
+@property (weak, nonatomic) IBOutlet UILabel *totalLabel;
+@property (strong, nonatomic) NSTimer *pointTimer;
+@property (strong, nonatomic) NSTimer *totalTimer;
 
+@property int currentPoint;
+@property int currentTotal;
+
+@property (weak, nonatomic) IBOutlet UILabel *tierLabel;
+@property (weak, nonatomic) IBOutlet UILabel *ecoSaveLabel;
+@property (weak, nonatomic) IBOutlet UILabel *rewardsLabel;
 
 @end
 
@@ -32,7 +46,78 @@
     
     [FBRequestConnection startWithGraphPath:@"me" parameters:[NSMutableDictionary  dictionaryWithObjectsAndKeys:@"name,picture",@"fields", nil] HTTPMethod:@"GET" completionHandler:^(FBRequestConnection *connection, id result, NSError *error) {
         NSLog(@"%@",result);
+        NSDictionary *dictionary = result;
+        NSString *name = [[dictionary objectForKey:@"name"] uppercaseString];
+        _username.text = name;
+        
+        NSString *urlString = [dictionary valueForKeyPath:@"picture.data.url"];
+        [_profilePic setImageWithURL:[NSURL URLWithString:urlString]];
     }];
+    
+    [self initLabels];
+    [self initBottomLabels];
+
+    
+     _pointTimer = [NSTimer scheduledTimerWithTimeInterval:0.001 target:self selector:@selector(addNumbersToPoint) userInfo:nil repeats:YES];
+    
+       _totalTimer = [NSTimer scheduledTimerWithTimeInterval:0.001 target:self selector:@selector(addNumbersToTotal) userInfo:nil repeats:YES];
+    
+}
+
+- (void)initLabels {
+    _pointLabel.text = @"0";
+    _totalLabel.text = @"0";
+}
+
+- (void)initBottomLabels {
+    _tierLabel.alpha = 0;
+    _ecoSaveLabel.alpha = 0;
+    _rewardsLabel.alpha = 0;
+}
+
+- (void)addNumbersToPoint {
+    
+    if (_currentPoint < 1324) {
+        _currentPoint++;
+        _pointLabel.text = [NSString stringWithFormat:@"%d",_currentPoint];
+    } else {
+        [_pointTimer invalidate];
+    }
+    
+    if (_currentPoint > 100) {
+        [self animateBottomLabels];
+    }
+
+    
+}
+
+- (void)addNumbersToTotal {
+    
+    if (_currentTotal < 135600) {
+        _currentTotal = _currentTotal + 51;
+        _totalLabel.text = [NSString stringWithFormat:@"%d",_currentTotal];
+    } else {
+        [_totalTimer invalidate];
+    }
+    
+    
+}
+
+- (void)animateBottomLabels {
+    
+    /*[UIView animateWithDuration:1.0 animations:^{
+        _tierLabel.alpha = 1;
+        _ecoSaveLabel.alpha = 1;
+        _rewardsLabel.alpha = 1;
+    }];*/
+    
+    [UIView animateWithDuration:1.0 delay:0.0 options:UIViewAnimationOptionCurveEaseIn animations:^{
+        _tierLabel.alpha = 1;
+        _ecoSaveLabel.alpha = 1;
+        _rewardsLabel.alpha = 1;
+        
+    } completion:nil];
+    
 }
 
 - (void)didReceiveMemoryWarning
