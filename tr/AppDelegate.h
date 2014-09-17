67d73c8601855e8ca7d8f107d6dee42806104c93
klipnov <klipnov@gmail.com>
2014-09-17 15:35:40 +0800

    Initial Commit

---
+++ tr/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  tr
+//
+//  Created by Qays on 9/17/14.
+//  Copyright (c) 2014 Qays. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ tr/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  tr
+//
+//  Created by Qays on 9/17/14.
+//  Copyright (c) 2014 Qays. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
